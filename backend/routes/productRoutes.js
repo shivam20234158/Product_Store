223b66fff0c0503f8b9bd15d6f23656056f51b60
shivam20234158 <149950525+shivam20234158@users.noreply.github.com>
2025-05-31 23:01:51 +0530

    added rate limitation through arcject and done most of the backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,16 +1,18 @@
 import expres from "express";
 import {
-    createProducts,
-  getProducts,
-  
+    createProduct,
+    getProducts,
+    getProduct,
+    updateProduct,
+    deleteProduct,
 } from "../controllers/productController.js";
 
 const router = expres.Router();
 
 router.get("/", getProducts);
-// router.get("/:id", getProduct);
-router.post("/", createProducts);
-// router.put("/:id", updateProduct);
-// router.delete("/:id", deleteProduct);
+router.get("/:id", getProduct);
+router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,37 @@ app.use(cors());
 app.use(helmet()) //security
 app.use(morgan("dev"))//log the requests: GET /test 304 5.942 ms - -
 
+//apply arcjet rate limit
+app.use(async (req, res, next) => {
+    try {
+      const decision = await aj.protect(req, {
+        requested: 1, // specifies that each request consumes 1 token
+      });
+  
+      if (decision.isDenied()) {
+        if (decision.reason.isRateLimit()) {
+          res.status(429).json({ error: "Too Many Requests" });
+        } else if (decision.reason.isBot()) {
+          res.status(403).json({ error: "Bot access denied" });
+        } else {
+          res.status(403).json({ error: "Forbidden" });
+        }
+        return;
+      }
+  
+      // check for the bots
+      if (decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
+        res.status(403).json({ error: "A bot detected" });
+        return;
+      }
+  
+      next();
+    } catch (error) {
+      console.log("Arcjet error", error);
+      next(error);
+    }
+  });
+
 app.use("/api/products",productRoutes);
 
 async function initDB() {
