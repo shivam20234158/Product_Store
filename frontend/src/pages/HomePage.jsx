b2749a8c229e8e6bbb829252fcb0c6907d330145
raunak singh <raunaksingh@raunaks-MacBook-Air.local>
2025-06-06 02:53:17 +0530

    added delete and add option

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,8 @@
 import { useEffect } from "react";
 import { useProductStore } from "../store/useProductStore";
-import { PlusCircleIcon, RefreshCwIcon } from "lucide-react";
+import { PackageIcon, PlusCircleIcon, RefreshCwIcon } from "lucide-react";
 import ProductCard from "../components/ProductCard";
+import AddProductModal from "../components/AddProductModal";
 
 function HomePage() {
   const { products, loading, error, fetchProducts } = useProductStore();
@@ -13,7 +14,8 @@ function HomePage() {
   return (
     <main className="max-w-6xl mx-auto px-4 py-8">
       <div className="flex justify-between items-center mb-8">
-        <button className="btn btn-primary">
+        <button className="btn btn-primary"
+         onClick={() => document.getElementById("add_product_modal").showModal()}>
           <PlusCircleIcon className="size-5 mr-2" />
           Add Product
         </button>
@@ -26,7 +28,22 @@ function HomePage() {
         </button>
       </div>
 
+      <AddProductModal/>
+
       {error && <div className="alert alert-error mb-8">{error}</div>}
+      {products.length === 0 && !loading && (
+        <div className="flex flex-col justify-center items-center h-96 space-y-4">
+          <div className="bg-base-100 rounded-full p-6">
+            <PackageIcon className="size-12" />
+          </div>
+          <div className="text-center space-y-2">
+            <h3 className="text-2xl font-semibold ">No products found</h3>
+            <p className="text-gray-500 max-w-sm">
+              Get started by adding your first product to the inventory
+            </p>
+          </div>
+        </div>
+      )}
 
       {loading ? (
         <div className="flex justify-center items-center h-64">
